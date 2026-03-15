ab49821b790a8287b40073b889b8dd35299e61e8
Ayush-014 <ayushjoshi8445@gmail.com>
2026-03-15 21:58:55 +0530

    feat: message route setup

--- backend/src/routes/v1/index.route.js
+++ backend/src/routes/v1/index.route.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { authRouter } from "./auth.route.js";
+import { messageRouter } from "./message.route.js";
 
 export const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/message", messageRouter);
--- backend/src/routes/v1/message.route.js
+++ backend/src/routes/v1/message.route.js
@@ -3,11 +3,9 @@ import { protectedRoute } from "../../middlewares/protect.middleware.js";
 import { asyncHandler } from "../../utils/AsyncHandler.js";
 import { getMessagesController, getUsersForSidebarController, sendMessageController } from "../../controllers/message.controller.js";
 
-const messageRouter = express.Router();
+export const messageRouter = express.Router();
 
 messageRouter.get("/users", protectedRoute, asyncHandler(getUsersForSidebarController));
 messageRouter.get("/:id", protectedRoute, asyncHandler(getMessagesController));
 
 messageRouter.post("/send/:id", protectedRoute, asyncHandler(sendMessageController));
-
-export default messageRouter;
